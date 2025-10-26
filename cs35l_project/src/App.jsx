1afa9092a9a20f93a51bd78bd5f08c944641e6e0
aarnav-11 <aarnav11@g.ucla.edu>
2025-10-26 01:52:17 -0700

    added node modules

---
+++ cs35l_project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Searchbar from './components/Searchbar'
+
+function App() {
+  return(
+    <div className='app'>
+      <div className="searchbar">
+        <div>Searchbar</div>
+        <div>Search Results</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cs35l_project/src/components/Searchbar.jsx
@@ -0,0 +1,15 @@
+
+import "./Searchbar.css"
+function Searchbar(){
+    
+    return(
+        <>
+            <div className="searchbar">
+                <div>Searchbar</div>
+                <div>Search Results</div>
+            </div>
+        </>
+    );
+}
+
+export default Searchbar;
