e61a49a6d1ec2cdf27606c7b7a5f4dea8b225347
aarnav-11 <aarnav11@g.ucla.edu>
2025-10-26 22:54:30 -0700

    added the ability to add notes using a plus button. yet to add backend to save notes.

--- cs35l_project/src/App.jsx
+++ cs35l_project/src/App.jsx
@@ -1,13 +1,17 @@
+
 import { useState } from 'react'
 import './App.css'
 import Searchbar from './components/Searchbar'
+import AddNoteButton from "./components/AddNoteButton"
 
 function App() {
   return(
     <div className='app'>
-      <div className="searchbar-container">
+      <div className="header-container">
         <Searchbar/>
+        <AddNoteButton/>
       </div>
+      
     </div>
   );
 }
