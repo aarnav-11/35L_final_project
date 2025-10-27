a94cfdca3a3d4efc28b323baadbc785bc47eb5c0
aarnav-11 <aarnav11@g.ucla.edu>
2025-10-27 11:50:36 -0700

    just added some filler notes (they shouldnt be hard coded in app.jsx anyways)

--- cs35l_project/src/App.jsx
+++ cs35l_project/src/App.jsx
@@ -11,6 +11,13 @@ function App() {
         <Searchbar/>
         <AddNoteButton/>
       </div>
+      <div className='notes-grid'>
+        <div className='note-card'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem sunt architecto hic similique tempore aperiam cumque laborum dicta iste eos recusandae obcaecati ullam magni, vero sint doloremque at exercitationem debitis!</div>
+        <div className='note-card'>bcaadaasadjemndjek</div>
+        <div className='note-card'>c</div>
+        <div className='note-card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nostrum nihil quis, aliquam mollitia nemo velit modi magni, reiciendis sequi nam alias minus cupiditate consectetur fugiat eos voluptatibus. Voluptatibus, sit?</div>
+        <div className='note-card'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi totam, optio quis, facere voluptatum modi aut asperiores culpa soluta molestiae eos maxime eveniet cupiditate quia minus. Assumenda, fugit. Praesentium, aliquid!</div>
+      </div>
       
     </div>
   );
