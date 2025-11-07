20b41efc765af648bc6604049e98d25cdfd61a48
aarnav-11 <aarnav11@g.ucla.edu>
2025-11-07 13:56:03 -0800

    Added an authentication page without logic

--- cs35l_project/src/App.jsx
+++ cs35l_project/src/App.jsx
@@ -1,12 +1,14 @@
 
 import MainPage from './pages/MainPage';
 import ErrorPage from './pages/404page';
+import Authentication from "./pages/Authentication"
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return(
     <Routes>
-      <Route path='/' element = { <MainPage/> }/>
+      <Route path='/' element = { <Authentication/> }/>
+      <Route path='/home' element = { <MainPage/> }/>
       <Route path = "*" element = { <ErrorPage/> }/>
     </Routes>
   );
---
+++ cs35l_project/src/pages/Authentication.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import "./Authentication.css";
+
+function Authentication(){
+    return(
+        <div className="mainBox">
+            <div className="auth">
+                Authencitcation
+            </div>
+            <div className="body"> 
+                <div>Email ID</div>
+                <div>Password</div>
+            </div>
+           
+        </div>
+    );
+}
+
+export default Authentication;
