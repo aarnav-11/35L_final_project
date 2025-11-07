8a0dfe49bdcced4e3fedf3d5e01ac8edf7585a33
aarnav-11 <aarnav11@g.ucla.edu>
2025-11-07 13:04:44 -0800

    added a kinda funny easter egg 404 page

--- cs35l_project/src/pages/404page.jsx
+++ cs35l_project/src/pages/404page.jsx
@@ -1,12 +1,15 @@
 
 import "./404page.css";
+import mindBlown from "../assets/mind_blown.avif"
 
 
 function ErrorPage(){
     return(
-        <div>
+        <div className="errorPage">
             <h1>404 page not found</h1>
             <p2>Our mind went blank :(</p2>
+            <b></b>
+            <img src={ mindBlown } height={250}></img>
         </div>
     )
 }
