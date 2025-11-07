55eb6cd150a6e9f007a29640a56db8119525c38d
aarnav-11 <aarnav11@g.ucla.edu>
2025-11-07 15:12:43 -0800

    added link from auth to home page

--- cs35l_project/src/pages/Authentication.jsx
+++ cs35l_project/src/pages/Authentication.jsx
@@ -1,18 +1,26 @@
 
 import React from "react";
 import "./Authentication.css";
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Authentication(){
+    const navigate = useNavigate();
     return(
         <div className="mainBox">
             <div className="auth">
-                Authencitcation
+                <h1>Unlock the full potential of your mind</h1>
             </div>
-            <div className="body"> 
-                <div>Email ID</div>
-                <div>Password</div>
+            <div className="authBody"> 
+                <div>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" className="username"></input>
+                </div>
+                <div>
+                    <label for="password">Password:</label>
+                    <input type="text" id="password" name="password" className="password"></input>
+                </div>
             </div>
-           
+            <button onClick={ () => navigate("/home")}>Sign Up</button>
         </div>
     );
 }
