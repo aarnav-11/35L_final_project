d8935274bb054922c8b60417644a46d8babd16ca
rseth06a <rseth06a@gmail.com>
2025-12-02 13:34:36 -0800

    link changed to clickable link

--- cs35l_project/src/components/Note.jsx
+++ cs35l_project/src/components/Note.jsx
@@ -10,7 +10,7 @@ function Note({ note, onRemoveNote }){
                 <h1>{note.title || "Untitled Thought"}</h1>
             </div>
             {/* if note is uploaded file show link, otherwise show text */}
-            {note.text && note.text.startsWith("http://localhost:3000/uploads") ? (
+            {note.text && (note.text.startsWith("http://") || note.text.startsWith("https://")) ? (
                 // <a 
                 //     href={note.text} 
                 //     target="_blank" 
@@ -20,7 +20,7 @@ function Note({ note, onRemoveNote }){
                 //     Open Uploaded File
                 // </a>
                 <a href={note.text} target="_blank" rel="noopener noreferrer">
-                    Open Uploaded File
+                    Click to Open
                 </a>
             ) : (
                 <p>{note.text}</p>
