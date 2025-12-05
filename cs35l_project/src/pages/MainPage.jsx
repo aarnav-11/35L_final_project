bc5da1344be8ab1d9e6e8051cac06f02ec69452b
aarnav-11 <aarnav11@g.ucla.edu>
2025-12-04 23:25:11 -0800

    Changes the background for all the pages

--- cs35l_project/src/pages/MainPage.jsx
+++ cs35l_project/src/pages/MainPage.jsx
@@ -133,7 +133,7 @@ function MainPage() {
 
   return(
     <div className='app'>
-        <Background colors={["#9BF267", "#C6FF8A", "#7AF2FF", "#4BC8FF", "#5570FF", "#A56BFF", "#FF76D6", "#FFB470"]} rotation={30} speed={0.3} scale={1.2} frequency={1.4} warpStrength={1.2} mouseInfluence={0.8} parallax={0.6} noise={0.08} transparent/>
+        <Background />
         <div className='navigation'>
         <Navigation/>
       </div>
--- cs35l_project/src/pages/Spaces.jsx
+++ cs35l_project/src/pages/Spaces.jsx
@@ -9,7 +9,7 @@ function Spaces(){
 
     return (
         <div className="spaces-container">
-            <Background colors={["#9BF267", "#C6FF8A", "#7AF2FF", "#4BC8FF", "#5570FF", "#A56BFF", "#FF76D6", "#FFB470"]} rotation={30} speed={0.3} scale={1.2} frequency={1.4} warpStrength={1.2} mouseInfluence={0.8} parallax={0.6} noise={0.08} transparent/>
+            <Background />
             <Navigation/>
             <h1 className="title">Spaces</h1>
             <hr></hr>
